12e030ca345e3c1af5c6b80d9db6f76ad4f80c99
Moustapha Amroune <amroune.moustapha@gmail.com>
2026-01-02 11:24:16 +0100

    feat : add currency converter modal layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div class="background"></div>
+      <div className="background">
+        <div className="modal">
+          <h1>Currency Converter</h1>
+          <label>USD to EUR Conversion</label>
+          <input type="number" />
+          <label>Start Currency :</label>
+          <select>
+            <option>USD</option>
+            <option>EUR</option>
+            <option>GBP</option>
+            <option>JPY</option>
+          </select>
+          <label>Target Currency :</label>
+          <select>
+            <option>USD</option>
+            <option>EUR</option>
+            <option>GBP</option>
+            <option>JPY</option>
+          </select>
+          <p>Converted Amount : </p>
+        </div>
+      </div>
     </>
   );
 }
